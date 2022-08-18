cead3bbe73ef4ff835caf6c6f777db8948ba72d4
davershp <110553852+davershp@users.noreply.github.com>
2022-08-18 14:33:20 -0400

    Updated FlaskApp.py to final

--- FlaskApp.py
+++ FlaskApp.py
@@ -26,19 +26,19 @@ top_ten_devs = pd.read_sql('SELECT * FROM Top_Ten_Played_Developers', engine)
 print(top_ten_devs)
 
 counts = pd.read_sql('SELECT * FROM Developer_Released_Games_By_Year', engine)
-print(counts)
+print(counts.columns)
 
 
 
 
 #--------------------------------------------------------------------------- Visualization 4 --------------------------------------------------
 
-fig,ax=plt.subplots(figsize=(10,10))
-ax=sns.set_style(style="darkgrid")
+#fig,ax=plt.subplots()
+#ax=sns.set_style(style="darkgrid")
 
 #------------------------------------------------ 
-x=[i for i in range(100)]
-y=[i for i in range(100)]
+#x=[i for i in range(100)]
+#y=[i for i in range(100)]
 #------------------------------------------------
 
 app=Flask(__name__, template_folder='BAH-TEDE-Project')
@@ -47,79 +47,81 @@ app=Flask(__name__, template_folder='BAH-TEDE-Project')
 def home():
     return render_template('index2.html')  
 
-@app.route('/visualize') 
-def visualize():
-    sns.lineplot(x,y)
-    canvas=FigureCanvas(fig)
-    img=io.BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    return send_file(img,mimetype='img/png')
+# @app.route('/visualize') 
+# def visualize():
+#     sns.lineplot(x,y)
+#     canvas=FigureCanvas(fig)
+#     img=io.BytesIO()
+#     fig.savefig(img)
+#     img.seek(0)
+#     return send_file(img,mimetype='img/png')
 
-#--------------------------------------------------------------------------- Visualization 3 --------------------------------------------------
+# #--------------------------------------------------------------------------- Visualization 3 --------------------------------------------------
 
-#fig,ax=plt.subplots(figsize=(10,10))
-#ax=sns.set_style(style="darkgrid")
-
-#------------------------------------------------ 
-m=[i for i in range(75)]
-n=[i for i in range(75)]
-#------------------------------------------------
+# fig2,ax2=plt.subplots()
+# ax2=sns.set_style(style="darkgrid")
+# #------------------------------------------------ 
+# m=[i for i in range(100)]
+# n=[i for i in range(100)]
+# #------------------------------------------------
  
 
-@app.route('/viz3') 
-def viz3():
-    sns.lineplot(m,n)
-    canvas=FigureCanvas(fig)
-    img=io.BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    return send_file(img,mimetype='img/png')
+# @app.route('/viz3') 
+# def viz3():
+#     sns.lineplot(m,n)
+#     canvas=FigureCanvas(fig2)
+#     img=io.BytesIO()
+#     fig2.savefig(img)
+#     img.seek(0)
+#     return send_file(img,mimetype='img/png')
  
-#--------------------------------------------------------------------------- Visualization 2 -s-------------------------------------------------
+# #--------------------------------------------------------------------------- Visualization 2 ---------------------------------------------------
 
-
-#------------------------------------------------ 
-q=[i for i in range(50)]
-w=[i for i in range(50)]
-#------------------------------------------------
+# fig3,ax3=plt.subplots()
+# ax3=sns.set_style(style="darkgrid")
+# #------------------------------------------------ 
+# q=[i for i in range(100)]
+# w=[i for i in range(100)]
+# #------------------------------------------------
  
 
-@app.route('/viz2') 
-def viz2():
-    sns.lineplot(q,w)
-    canvas=FigureCanvas(fig)
-    img=io.BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    return send_file(img,mimetype='img/png')
+# @app.route('/viz2') 
+# def viz2():
+#     sns.lineplot(q,w)
+#     canvas3=FigureCanvas(fig3)
+#     img3=io.BytesIO()
+#     fig3.savefig(img3)
+#     img3.seek(0)
+#     return send_file(img3,mimetype='img/png')
 #--------------------------------------------------------------------------- # Visualization 1 --------------------------------------------------
 
 
 #------------------------------------------------ 
-e=[i for i in range(100)]
-r=[i for i in range(100)]
+#e=[i for i in range(100)]
+#r=[i for i in range(100)]
 #------------------------------------------------
 
-fig,ax=plt.subplots(figsize=(5,5))
-ax=sns.set_style(style="darkgrid")
+fig4,ax4=plt.subplots()
+ax4=sns.set_style(style="darkgrid")
 
 @app.route('/viz1') 
 def viz1():
-    sns.lineplot(e,r)   
-    canvas=FigureCanvas(fig)
-    img=io.BytesIO()
-    plt.savefig(img)
-    img.seek(0)
+    #sns.lineplot(e,r)  
     plt.title('Games Released By Year')
 
     plt.xlabel('Year')
 
     plt.ylabel('Number of Games')
 
-    sns.lineplot(x='release_year', y='Count', hue='developer', data=counts)
+    sns.lineplot(x='release_year', y='Count', hue='developer', data=counts) 
 
-    plt.show()
+    canvas=FigureCanvas(fig4)
+    img=io.BytesIO()
+    fig4.savefig(img)
+    img.seek(0)
+
+
+    #plt.show()
     return send_file(img,mimetype='img/png')
 
 
@@ -127,3 +129,4 @@ def viz1():
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
